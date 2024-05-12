f6bebdf0e1b97c66335d493f6207c7fa9f2e17dc
Pooja Singh <106243903+PoojaSingh0013@users.noreply.github.com>
2024-05-12 07:54:35 +0000

    DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=33
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let score1="33abs"
+let valueInNumber=Number(score1)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1--->True ,  0--->False
+// "" => false
+// "Rani" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
